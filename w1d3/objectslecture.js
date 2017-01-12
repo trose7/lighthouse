4dab341b45cf969ca21ee4366865413ed529e1a5
trose7 <trevor.l.rose1@gmail.com>
2017-01-12 23:12:11 +0000

    Waldo

---
+++ w1d3/objectslecture.js
@@ -0,0 +1,46 @@
+// Boolean
+
+var tval = true
+var fval = false
+
+//string
+
+var myStr = '"Hi"';
+var myOtherStr = "'Hello'"
+var mty = '' // empty string
+var numStr = '123' //this is also a string
+
+//check that both the types and values of these variables are the same
+// with three equal signs
+
+if (mty === fval) {
+
+}
+
+// Number
+
+var intergerNumber= 1;
+var nonIntergerNumber = 1.1
+var zero = 0;
+var negzero = -0;
+var inf = Infinity;
+var negIng = -Infinity;
+var nana = Nan; // is of type number. Value =
+
+// Null
+
+var nullVariable = undefined;
+
+//undefined
+
+var undefunedVar= undefined
+var undefinedVar2;
+
+// Everything else is an object !!!
+
+var myObj = {};
+var myArr = [];  //Array
+
+
+
+
